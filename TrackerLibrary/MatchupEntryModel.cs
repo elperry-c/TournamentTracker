6ade303db7d5f3537ea3591c69c2adc6a2926e1b
elperry-c <elperry.contact@gmail.com>
2021-06-11 20:48:54 +0900

    ready to design

--- TrackerLibrary/MatchupEntryModel.cs
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -6,24 +6,28 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one team in a matchup
+    /// </summary>
+    
     class MatchupEntryModel
     {
         /// <summary>
         /// Represent one team in the matchup.
         /// </summary>
+        
         public TeamModel TeamCompeting { get; set; }
+
         /// <summary>
         /// Represents the score for this particular team.
         /// </summary>
+        
         public double Score { get; set; }
-        /// <summary>
-        /// Represents the matchup that this team came from as the winner
-        /// </summary>
-        public MatchupModel ParentMatchup { get; set; }
 
-        public MatchupEntryModel(double initialScore)
-        {
-            Console.WriteLine();
-        }
+        /// <summary>
+        /// Represents the matchup that this team came  from as the winner
+        /// </summary>
+        
+        public MatchupModel ParentMatchup { get; set; }
     }
 }
--- TrackerLibrary/MatchupModel.cs
+++ TrackerLibrary/MatchupModel.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one match in the tournament.
+    /// </summary>
+    
     class MatchupModel
     {
+        /// <summary>
+        /// The set of teams that were involved in this match.
+        /// </summary>
+        
         public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
-        public TeamModel teamModel { get; set; }
+
+        /// <summary>
+        /// The winner of the match.
+        /// </summary>
+        
+        public TeamModel Winner { get; set; }
+
+        /// <summary>
+        /// Which round this match is a part of.
+        /// </summary>
+        
         public int MatchupRound { get; set; }
     }
 }
